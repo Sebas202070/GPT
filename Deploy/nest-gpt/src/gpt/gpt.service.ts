c0c8a630ae34988ef567642845783314349dad55
Sebastián Rodrigo Najle <103438231+Sebas202070@users.noreply.github.com>
2024-12-05 18:23:29 -0300

    Commit Ok

--- Deploy/nest-gpt/src/gpt/gpt.service.ts
+++ Deploy/nest-gpt/src/gpt/gpt.service.ts
@@ -6,8 +6,7 @@ import { OrthographyDto } from './dtos/orthography.dto';
 @Injectable()
 export class GptService {
   private openai = new OpenAI({
-    apiKey:
-      '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',
+    apiKey: process.env.OPEN_AI_KEY,
   });
   async orthograpyCheck(orthographyDto: OrthographyDto) {
     return await UsecasesOrthography(this.openai, {
