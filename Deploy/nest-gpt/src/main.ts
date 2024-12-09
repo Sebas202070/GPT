bef3d6848408961135d76ac69fc9932defedc10b
Sebastián Rodrigo Najle <103438231+Sebas202070@users.noreply.github.com>
2024-12-09 13:33:27 -0300

    CORSOK

--- Deploy/nest-gpt/src/main.ts
+++ Deploy/nest-gpt/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,13 +11,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept',
-  });
-  app.use(cookieParser());
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
